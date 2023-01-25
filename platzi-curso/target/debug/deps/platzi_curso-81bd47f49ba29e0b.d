c619279236c39f79bd9a74805fceaae0bd9a5b76
Oscar Bucio <oscarbucio2001@gmail.com>
2023-01-25 01:49:08 -0600

    first commits

---
+++ platzi-curso/target/debug/deps/platzi_curso-81bd47f49ba29e0b.d
@@ -0,0 +1,5 @@
+/home/dany0343/dev/cursoDeRust/platzi-curso/target/debug/deps/platzi_curso-81bd47f49ba29e0b.rmeta: src/main.rs
+
+/home/dany0343/dev/cursoDeRust/platzi-curso/target/debug/deps/platzi_curso-81bd47f49ba29e0b.d: src/main.rs
+
+src/main.rs:
