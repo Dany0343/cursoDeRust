41e4bc89e077420e4565b2c45e924894b147241f
Oscar Bucio <oscarbucio2001@gmail.com>
2023-01-25 01:54:11 -0600

    first commits

---
+++ platzi-curso/target/debug/deps/platzi_curso-0995a1ba94ae6bf7.d
@@ -0,0 +1,5 @@
+/home/dany0343/dev/cursoDeRust/platzi-curso/target/debug/deps/platzi_curso-0995a1ba94ae6bf7: src/main.rs
+
+/home/dany0343/dev/cursoDeRust/platzi-curso/target/debug/deps/platzi_curso-0995a1ba94ae6bf7.d: src/main.rs
+
+src/main.rs:
---
+++ platzi-curso/target/debug/platzi-curso.d
@@ -0,0 +1,1 @@
+/home/dany0343/dev/cursoDeRust/platzi-curso/target/debug/platzi-curso: /home/dany0343/dev/cursoDeRust/platzi-curso/src/main.rs
