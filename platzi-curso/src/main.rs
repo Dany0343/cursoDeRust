1afa5f94d390820b5aff8f05fcea90feaf678797
Oscar Bucio <oscarbucio2001@gmail.com>
2023-01-29 23:03:05 -0600

    first project done

--- platzi-curso/src/main.rs
+++ platzi-curso/src/main.rs
@@ -1,27 +1,115 @@
+use regex::Regex;
+
 fn main() {
-    // Dos numeros que se van a sumar
-    let num_1 = 120; // Como es obvio que la variable tiene un valor numerico, no es necesario ponerle el tipo, Rust automaticamente lo asigna.
-    let num_2 = 321;
 
-    let suma = num_1 + num_2;
+    // Regex
+    // (\d+)\s?\+\s?(\d+)
+    let re_add = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
+    let re_mult = Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
+    let re_rest = Regex::new(r"(\d+)\s?\-\s?(\d+)").unwrap();
+    let re_div = Regex::new(r"(\d+)\s?\-\s?(\d+)").unwrap();
 
+
+
+
+    // Traer datos del usuario
+    println!("Por favor introduce tu expresión: ");  
+    let mut expression = String::new();
+    std::io::stdin().read_line(&mut expression).unwrap();
+    println!("{:?}", expression); 
+    
+    // Multiplicación
     loop {
-        // Mostrar los dos números en pantalla
-        println!("Por favor escribir la suma de {} y {}:", num_1, num_2);
+        // Aplicar Operaciones
+        let caps = re_mult.captures(expression.as_str());
 
-        // Obtener del usuario el número que representa la suma
-        let mut suma_usuario = String::new();
-        std::io::stdin().read_line(&mut suma_usuario).unwrap();
-
-        let suma_usuario_int: i16 = suma_usuario.trim().parse().unwrap();
-
-        if suma_usuario_int == suma {
-            println!("Lo has hecho muy bien, el resultado es correcto");
-            break;
-        }
-        else {
-            println!("El resultado {} no es correcto, por favor intentalo de nuevo", suma_usuario_int);
+        if caps.is_none() {
+            break
         }
 
+        let caps = caps.unwrap();
+
+        println!("{:?}", caps);
+        
+        let cap_expression = caps.get(0).unwrap().as_str();
+        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+        let mult = left_value * right_value;
+
+        expression = expression.replace(cap_expression, &mult.to_string());
+        println!("{:?}", expression); 
+        
     }
+
+    // Division
+    loop {
+        // Aplicar Operaciones
+        let caps = re_div.captures(expression.as_str());
+
+        if caps.is_none() {
+            break
+        }
+
+        let caps = caps.unwrap();
+
+        println!("{:?}", caps);
+        
+        let cap_expression = caps.get(0).unwrap().as_str();
+        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+        let mult = left_value / right_value;
+
+        expression = expression.replace(cap_expression, &mult.to_string());
+        println!("{:?}", expression); 
+        
+    }
+
+
+    // Suma
+    loop {
+        // Aplicar Operaciones
+        let caps = re_add.captures(expression.as_str());
+
+        if caps.is_none() {
+            break
+        }
+
+        let caps = caps.unwrap();
+
+        println!("{:?}", caps);
+        
+        let cap_expression = caps.get(0).unwrap().as_str();
+        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+        let addition = left_value + right_value;
+
+        expression = expression.replace(cap_expression, &addition.to_string());
+        println!("{:?}", expression); 
+        
+    }
+
+    // Resta
+    loop {
+        // Aplicar Operaciones
+        let caps = re_rest.captures(expression.as_str());
+
+        if caps.is_none() {
+            break
+        }
+
+        let caps = caps.unwrap();
+
+        println!("{:?}", caps);
+        
+        let cap_expression = caps.get(0).unwrap().as_str();
+        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+        let rest = left_value - right_value;
+
+        expression = expression.replace(cap_expression, &rest.to_string());
+        println!("{:?}", expression); 
+        
+    }
+    // Mostrar resultado
+    println!("Resultado {}", expression);
 }
