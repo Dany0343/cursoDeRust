c859536efd67458d0a461e3e050829a1dbbd6ba7
Oscar Bucio <oscarbucio2001@gmail.com>
2023-02-02 13:30:46 -0600

    vector and for

--- platzi-curso/src/main.rs
+++ platzi-curso/src/main.rs
@@ -1,12 +1,3 @@
 fn main() {
-    // Vectores
-    let mut nombres: Vec<String> = Vec::new();
     
-    println!("Por favor, introduce un nombre: ");
-    let mut nombre = String::new();
-    std::io::stdin().read_line(&mut nombre).unwrap();
-
-    nombres.push(nombre);
-
-    println!("{:?}", nombres); // Mostrar Vector de manera entera con el truco
 }
