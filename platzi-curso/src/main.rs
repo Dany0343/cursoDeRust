c800a75eff5c7b22c3e0e29842298a3221480067
Oscar Bucio <oscarbucio2001@gmail.com>
2023-02-02 13:51:20 -0600

    functions done

--- platzi-curso/src/main.rs
+++ platzi-curso/src/main.rs
@@ -1,3 +1,19 @@
+fn sumar_uno(numero_a_sumar: i16) -> i16 {  // Como en Python así se especifica el tipo que se va a devolver
+
+    let numero_final = numero_a_sumar + 1;
+
+    println!("{}", numero_final);
+
+    return numero_final;
+}
+
+
+
 fn main() {
-    
+    println!("Holaaa");
+
+    let diez_mas_uno: i16 = sumar_uno(10);
+    sumar_uno(diez_mas_uno);
+    sumar_uno(12);
+
 }
