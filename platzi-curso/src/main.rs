e9a5162e1cfa07961e6db6996b4ae97889740b68
Oscar Bucio <oscarbucio2001@gmail.com>
2023-02-03 00:34:05 -0600

    final project started

--- platzi-curso/src/main.rs
+++ platzi-curso/src/main.rs
@@ -1,7 +1,7 @@
 use csv::{ReaderBuilder, StringRecord}; // Esto permite leer un String que tenga forma de CSV
 use std::{fs}; // Para poder traernos el archivo al código
 
-const FILENAME: &str = "./history.csv";
+const FILENAME: &str = "history.csv";
 fn main() {
 
     let content = fs::read_to_string(FILENAME).unwrap();
